59456f2d61bff654863061a894ac3ac9c5f177ed
AkshitaTomar1 <axtomar7@gmail.com>
2024-07-26 13:03:24 +0530

    model trainer added

--- src/DimondPricePrediction/pipelines/training_pipeline.py
+++ src/DimondPricePrediction/pipelines/training_pipeline.py
@@ -1,6 +1,7 @@
 
 from src.DimondPricePrediction.components.data_ingestion import DataIngestion
 from src.DimondPricePrediction.components.data_transformation import DataTransformation
+
 from src.DimondPricePrediction.components.model_trainer import ModelTrainer
 
 import os
